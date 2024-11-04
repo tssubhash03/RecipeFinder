a4e3226a9d933eb302adfde79e4ef6c13404948d
tssubhash03 <subhash03ts2004@gmail.com>
2024-11-04 17:51:28 +0530

    commit:2 Added feature of searchBarResult

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+let availableKeywords = [
+    'HTML', 'CSS', 'JavaScript', 'Framework', 'API', 'Bootstrap', 'React', 'Node', 'AJAX', 'Frontend', 'Backend', 'Database', 'JSON', 'jQuery', 'Vue', 'Sass', 'Webpack', 'SEO', 'Responsive', 'Hosting'
+]
+const resulBox=document.querySelector(".result-box");
+const inputBox=document.getElementById("search-input");
+
+document.getElementById('clear-btn').addEventListener('click', closefunction);
+
+function closefunction(){
+    inputBox.value='';
+}
+inputBox.onkeyup=function(){
+    let res=[];
+    let input=inputBox.value;
+    if(input.length){
+        res=availableKeywords.filter((keyword)=>{
+           return  keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(res);
+    }
+    display(res);
+    if(!res.length){
+        resulBox.innerHTML='';
+    }
+    
+}
+function display(res){
+    const content=res.map((list)=>{
+        return "<li onclick=select(this)>" +list+ "</li>";
+    });
+    resulBox.innerHTML = "<ul>"+content.join('')+"</ul>";
+}
+function select(list){
+    inputBox.value=list.innerHTML;
+    resulBox.innerHTML='';
+}
+
+
 document.getElementById('search-btn').addEventListener('click', getMealList);
 function getMealList() {
     let searchInputTxt=document.getElementById('search-input').value.trim();
